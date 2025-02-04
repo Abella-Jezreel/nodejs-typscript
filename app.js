e4bb6a3cb6fa8e4e0cf328c21056d73521e88756
Abella-Jezreel <teamabella@outlook.com>
2025-02-04 17:58:30 +0800

    Working with Union Type

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var num1Element = document.getElementById("num1");
 var num2Element = document.getElementById("num2");
 var buttonElement = document.querySelector("button");
 function add(a, b) {
-    return a + b;
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    }
+    else if (typeof a === "string" && typeof b === "string") {
+        return a + "" + b;
+    }
+    return +a + +b;
 }
 buttonElement.addEventListener("click", function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
-    var result = add(+num1, +num2);
-    console.log(result);
+    var result1 = add(num1, num2);
+    var result2 = add(+num1, +num2);
+    console.log(result1);
+    console.log(result2);
 });
--- app.ts
+++ app.ts
@@ -2,14 +2,21 @@ const num1Element = document.getElementById("num1")! as HTMLInputElement;
 const num2Element = document.getElementById("num2")! as HTMLInputElement;
 const buttonElement = document.querySelector("button")!;
 
-function add(a: number, b: number) {
-  return a + b;
+function add(a: number | string, b: number | string) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else if (typeof a === "string" && typeof b === "string") {
+    return a + " " + b;
+  }
+  return +a + +b;
 }
 
 buttonElement.addEventListener("click", function () {
   const num1 = num1Element.value;
   const num2 = num2Element.value;
 
-  const result = add(+num1, +num2);
-  console.log(result);
+  const result1 = add(num1, num2);
+  const result2 = add(+num1, +num2);
+  console.log(result1);
+  console.log(result2);
 });
