d26fe02965e341807a440812c0c5ec9f9f840c35
Abella-Jezreel <teamabella@outlook.com>
2025-02-06 16:46:17 +0800

    Finishing The REST Routes

--- routes/todos.ts
+++ routes/todos.ts
@@ -18,4 +18,30 @@ router.post("/todo", (req, res, next) => {
   res.status(201).json({ message: "Added Todo", todo: newTodo });
 });
 
+router.put("/todo/:todoId", (req: any, res: any, next) => {
+  const tid = req.params.todoId;
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    return res
+      .status(200)
+      .json({ message: "Updated Todo", todo: todos[todoIndex] });
+  }
+
+  res.status(404).json({ message: "Could not find todo for this id." });
+});
+
+router.delete("/todo/:todoId", (req: any, res: any, next) => {
+  const tid = req.params.todoId;
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+
+  if (todoIndex >= 0) {
+    todos.splice(todoIndex, 1);
+    return res.status(200).json({ message: "Deleted Todo" });
+  }
+
+  res.status(404).json({ message: "Could not find todo for this id." });
+});
+
 export default router;
